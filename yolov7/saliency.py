9ef5881b6da4f3ab7e3f5cfd8e2657cd50b6d6a0
schanumolu <32376031+scrs22@users.noreply.github.com>
2022-12-12 15:14:54 -0500

    sliency

--- yolov7/saliency.py
+++ yolov7/saliency.py
@@ -80,6 +80,7 @@ image = cv2.resize(image,
 
 out = inference_detector(model, image)
 res = image.copy()
+dir(out)
 for i, pred in enumerate(out):
     for *box, score in pred:
         if score < 0.5:
