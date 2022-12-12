ef3bc2468d7d2cf8d0e91e0979ecb491ab8297a0
schanumolu <32376031+scrs22@users.noreply.github.com>
2022-12-12 12:57:16 -0500

    modified yolo2coco

--- yolov7/data/yolo2coco.py
+++ yolov7/data/yolo2coco.py
@@ -1,14 +1,15 @@
 from pylabel import importer
 
+folders=['train','test','val']
+for folder in folders:
+    path_to_annotations = f"dataset_single_folder_train_val_test_split/{folder}/labels"
 
-path_to_annotations = "dataset_single_folder/labels"
+    #Identify the path to get from the annotations to the images 
+    path_to_images = "../images"
 
-#Identify the path to get from the annotations to the images 
-path_to_images = "../images"
+    #Import the dataset into the pylable schema 
+    #Class names are defined here https://github.com/ultralytics/yolov5/blob/master/data/coco128.yaml
+    classes = (['scallop','herring','dead-scallop','flounder','roundfish','skate'])
+    dataset = importer.ImportYoloV5(path_to_annotations,"png",classes,path_to_images,f"{folder}")
 
-#Import the dataset into the pylable schema 
-#Class names are defined here https://github.com/ultralytics/yolov5/blob/master/data/coco128.yaml
-classes = (['scallop','herring','dead-scallop','flounder','roundfish','skate'])
-dataset = importer.ImportYoloV5(path_to_annotations,"png",classes,path_to_images,"annotations")
-
-dataset.export.ExportToCoco(cat_id_index=1)
+    dataset.export.ExportToCoco(cat_id_index=1)
