df799090e871f905ca9d036851af57fa22b2cf13
schanumolu <32376031+scrs22@users.noreply.github.com>
2022-12-11 22:29:02 -0500

    yolo2coco

--- yolov7/data/yolo2coco.py
+++ yolov7/data/yolo2coco.py
@@ -9,6 +9,6 @@ path_to_images = "/home/ubuntu/670_fish_project/yolov7/data/dataset_single_folde
 #Import the dataset into the pylable schema 
 #Class names are defined here https://github.com/ultralytics/yolov5/blob/master/data/coco128.yaml
 yoloclasses = ['scallop','herring','dead-scallop','flounder','roundfish','skate' ]
-dataset = importer.ImportYoloV5(path=path_to_annotations, path_to_images=path_to_images, cat_names=yoloclasses,)
+dataset = importer.ImportYoloV5(path_to_annotations,"png",yoloclasses,path_to_images,"annotations")
 
 dataset.export.ExportToCoco(cat_id_index=1)
