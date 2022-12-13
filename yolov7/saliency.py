3027852d61bb60be4e6a699d6da5309cd5855105
schanumolu <32376031+scrs22@users.noreply.github.com>
2022-12-12 19:46:34 -0500

    sliency

--- yolov7/saliency.py
+++ yolov7/saliency.py
@@ -58,7 +58,7 @@ def generate_saliency_map(image,
         bboxes,scores=preds.bboxes.cpu().data.numpy(),preds.scores.cpu().data.numpy()
         temp=[]
         for i, bbox in enumerate(bboxes):
-            if scores[i] < 0.2577:
+            if scores[i] < 0.26356:
                 break
             box = tuple(np.round(bbox).astype(int).tolist())
             temp.append(iou(target_box, box) * scores[i])
@@ -85,17 +85,17 @@ image = cv2.resize(image,
                   interpolation=cv2.INTER_AREA)
 
 
-out = inference_detector(model, image)
-res = image.copy()
-preds=out.pred_instances
-bboxes,labels,scores=preds.bboxes.cpu().data.numpy(),preds.labels.cpu().data.numpy(),preds.scores.cpu().data.numpy()
-for i, bbox in enumerate(bboxes):
-    if scores[i] < 0.1:
-        break
-    box = tuple(np.round(bbox).astype(int).tolist())
-    print(i, labels[i], box, scores[i])
-    cv2.rectangle(res, box[:2], box[2:], (0, 255, 0), 5)
-# plt.figure(figsize=(7, 7))
+# out = inference_detector(model, image)
+# res = image.copy()
+# preds=out.pred_instances
+# bboxes,labels,scores=preds.bboxes.cpu().data.numpy(),preds.labels.cpu().data.numpy(),preds.scores.cpu().data.numpy()
+# for i, bbox in enumerate(bboxes):
+#     if scores[i] < 0.1:
+#         break
+#     box = tuple(np.round(bbox).astype(int).tolist())
+#     print(i, labels[i], box, scores[i])
+#     cv2.rectangle(res, box[:2], box[2:], (0, 255, 0), 5)
+# # plt.figure(figsize=(7, 7))
 # plt.imshow(res[:, :, ::-1])
 # plt.show()
 
